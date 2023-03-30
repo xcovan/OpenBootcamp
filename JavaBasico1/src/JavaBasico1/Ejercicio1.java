bbc809aa2bc8dd71074f7931aab836ae25e1c007
NicoPS <119110500+xcovan@users.noreply.github.com>
2023-03-30 14:02:32 +0200

    Add files via upload

---
+++ JavaBasico1/src/JavaBasico1/Ejercicio1.java
@@ -0,0 +1,24 @@
+package JavaBasico1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ejercicio 1 de curso Java Básico, imprimiendo tipos de datos: ");
+        //Numéricos
+        byte a =1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+        float e = 4.1f;
+        double f = 5.2;
+        //booleano
+        boolean g = true;
+        //caracteres
+        char h = 'h';
+        String i = "Hola";
+
+        //Imprimiendo datos:
+        System.out.println("Byte: " + a +"\nShort: "+b+"\nInt: "+c+"\nLong: "+d+"\nFloat: "+e+"\nDouble: "+f);
+        System.out.println(g+"\nChar: " +h+"\nString: "+i);
+    
+    }
+}
